b014bf806aabfb6f59199b369030c82987a8338d
Philougamer014 <158100794+Philougamer014@users.noreply.github.com>
2024-04-29 16:17:04 -0400

    ajout du fichier java script

---
+++ estore.comtp4/public/javascripts/script.js
@@ -0,0 +1,38 @@
+//<tr>
+//    <td class="checkout-product">
+//        <div class="product-cart d-flex">
+//            <div class="product-thumb">
+//                <img src="assets-donottouch/images/product-cart/product-1.png"
+//                    alt="Product">
+//            </div>
+//            <div class="product-content media-body">
+//                <h5 class="title">
+//                    <a href="#">Hollow Port</a>
+//                </h5>
+//                <ul>
+//                    <li><span>Brown</span></li>
+//                    <li><span>XL</span></li>
+//                    <li>
+//                        <a class="delete" href="javascript:void(0)">
+//                            <i class="mdi mdi-delete"></i>
+//                        </a>
+//                    </li>
+//                </ul>
+//            </div>
+//        </div>
+//    </td>
+//    <td class="checkout-quantity">
+//        <div class="product-quantity d-inline-flex">
+//            <button type="button" id="sub" class="sub">
+//                <i class="mdi mdi-minus"></i>
+//            </button>
+//            <input type="text" value="1">
+//            <button type="button" id="add" class="add">
+//                <i class="mdi mdi-plus"></i>
+//            </button>
+//        </div>
+//    </td>
+//    <td class="checkout-price">
+//        <p class="price">$36.00</p>
+//    </td>
+//</tr>
