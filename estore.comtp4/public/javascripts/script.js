640c291fca7f02b92be582e67da5fa4070bdf26f
Philougamer014 <158100794+Philougamer014@users.noreply.github.com>
2024-05-02 14:01:30 -0400

    Panier cassiment complèter

--- estore.comtp4/public/javascripts/script.js
+++ estore.comtp4/public/javascripts/script.js
@@ -1,85 +1,89 @@
-var totalPrice = 0;
-var totalAmouthArticle = 0;
-
-
 $(document).ready(function() {
+    // Ajout d'articles au panier
     $('.info-button').click(function() {
         var articleId = $(this).data('article-id');
-        var prix = $(this).data('prix');
+        var prix = parseFloat($(this).data('prix')); // Assurez-vous que ceci est bien un nombre
         var name = $(this).data('name');
 
-        console.log(articleId);
-        console.log(prix);
-        console.log(name);
-
-        totalPrice += prix;
-        totalAmouthArticle++;
-
-        // Création du HTML pour la nouvelle ligne dans le tbody avec la classe 'tbody-cart'
-        var newRowHtml = `
-            <tr>
-                <td class="checkout-product">
-                    <div class="product-cart d-flex">
-                        <div class="product-thumb">
-                            <img src="assets-donottouch/images/product-cart/product-1.png" alt="Product">
+        var existingRow = $('.tbody-cart tr[data-article-id="' + articleId + '"]');
+        if (existingRow.length > 0) {
+            var qtyInput = existingRow.find('input[type="text"]');
+            var newQuantity = parseInt(qtyInput.val()) + 1;
+            qtyInput.val(newQuantity);
+            existingRow.find('.price').text('$' + (prix * newQuantity).toFixed(2));
+        } else {
+            var newRowHtml = `
+                <tr data-article-id="${articleId}" data-prix="${prix}">
+                    <td class="checkout-product">
+                        <div class="product-cart d-flex">
+                            <div class="product-thumb">
+                                <img src="assets-donottouch/images/product-cart/product-${articleId}.png" alt="Product">
+                            </div>
+                            <div class="product-content media-body">
+                                <h5 class="title"><a href="#">${name}</a></h5>
+                                <ul>
+                                    <li><span>Brown</span></li>
+                                    <li><span>XL</span></li>
+                                    <li><a class="delete" href="javascript:void(0)"><i class="mdi mdi-delete"></i></a></li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="product-content media-body">
-                            <h5 class="title"><a href="#">${name}</a></h5>
-                            <ul>
-                                <li><span>Brown</span></li>
-                                <li><span>XL</span></li>
-                                <li><a class="delete" href="javascript:void(0)"><i class="mdi mdi-delete"></i></a></li>
-                            </ul>
+                    </td>
+                    <td class="checkout-quantity">
+                        <div class="product-quantity d-inline-flex">
+                            <button type="button" class="sub"><i class="mdi mdi-minus"></i></button>
+                            <input type="text" value="1">
+                            <button type="button" class="add"><i class="mdi mdi-plus"></i></button>
                         </div>
-                    </div>
-                </td>
-                <td class="checkout-quantity">
-                    <div class="product-quantity d-inline-flex">
-                        <button type="button" class="sub"><i class="mdi mdi-minus"></i></button>
-                        <input type="text" value="1">
-                        <button type="button" class="add"><i class="mdi mdi-plus"></i></button>
-                    </div>
-                </td>
-                <td class="checkout-price">
-                    <p class="price">$${prix}</p>
-                </td>
-            </tr>
-        `;
+                    </td>
+                    <td class="checkout-price">
+                        <p class="price">$${prix.toFixed(2)}</p>
+                    </td>
+                </tr>
+            `;
+            $('.tbody-cart').append(newRowHtml);
+        }
+        updateTotalPriceAndQuantity();
+    });
 
-        
-        $('.tbody-cart').append(newRowHtml);
+    // Gestion des boutons '+' et '-'
+    $('.tbody-cart').on('click', '.add, .sub', function() {
+        var $input = $(this).siblings('input[type="text"]');
+        var quantity = parseInt($input.val());
+        var $row = $(this).closest('tr');
+        var pricePerItem = parseFloat($row.data('prix')); // Récupération du prix par article
 
-        updateSubtotalPrice(totalPrice)
-        updateIconText(totalAmouthArticle); 
+        if ($(this).hasClass('add')) {
+            quantity++;
+        } else if (quantity > 1) {
+            quantity--;
+        }
+
+        $input.val(quantity);
+        $row.find('.price').text('$' + (quantity * pricePerItem).toFixed(2));
+
+        updateTotalPriceAndQuantity();
     });
 });
 
-function updateSubtotalPrice(newPrice) {
+function updateTotalPriceAndQuantity() {
+    var totalPrice = 0;
+    var totalQuantity = 0;
+    $('.tbody-cart tr').each(function() {
+        var price = parseFloat($(this).find('.price').text().substring(1));
+        var quantity = parseInt($(this).find('input[type="text"]').val());
+        totalPrice += price;
+        totalQuantity += quantity;
+    });
 
-    var priceElement = document.querySelector('.checkout-sub-total .price');
-    
-    
-    priceElement.textContent = `$${newPrice.toFixed(2)}`; 
+    updateSubtotalPrice(totalPrice);
+    updateIconText(totalQuantity);
+}
+
+function updateSubtotalPrice(newPrice) {
+    $('.checkout-sub-total .price').text(`$${newPrice.toFixed(2)}`);
 }
 
 function updateIconText(newValue) {
-   
-    var iconTextElement = document.querySelector('.icon-text.text-style-1');
-    
-
-    iconTextElement.textContent = newValue;
+    $('.icon-text.text-style-1').text(newValue);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
